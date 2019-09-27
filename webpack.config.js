0ebbcbf0911229f85ff9115525ec9736ef25a94b
VivianDoreen <nabulovivian2014@gmail.com>
2019-09-27 16:36:51 +0300

    chore(setup):create react app - install react, react-dom - install webpack, webpack-dev-server, webpack-cli - install babel-core, babel-loader, @babel/preset-env,@babel/preset-react - setup webpack.config.js - setup .babelrc - create index.html file - create index.js file [Maintains #168799900]

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'saas-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: true
+  }
+};
